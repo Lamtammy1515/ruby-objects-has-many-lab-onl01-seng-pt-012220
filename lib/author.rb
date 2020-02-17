b82c287f35cc849644dc30632d69edcfeefa7e32
Tammy Lam <lamtammy1515@gmail.com>
2020-02-17 01:17:51 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,8 @@
 class Author 
   attr_accessor :name  
-  @@counter = 0
+ 
   def initialize(name)
     @name = name 
-    @posts = [] 
   end 
   
   def posts 
@@ -13,13 +12,12 @@ class Author
   def add_post(name)
    self.posts << name 
    name.author = self 
-   @@counter += 1 
   end 
   
   def add_post_by_title(name)
    self.posts << name 
    name.author = self 
-   @@counter += 1
+  
   end 
 
   def self.post_count 
