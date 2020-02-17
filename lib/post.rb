e0064b15383e3c92c034dd679422008f1a643078
Tammy Lam <lamtammy1515@gmail.com>
2020-02-17 01:22:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -10,20 +10,13 @@ class Post
   def self.all
     @@all 
   end 
-  
-  def title
-    
-  end 
-  
-  def author
-    
-  end 
+
   
   def author_name
-    if @author == nil 
-      return nil 
+    if self.author 
+      self.author.name 
     else 
-      @author.name 
+      nil 
     end 
   end 
     
